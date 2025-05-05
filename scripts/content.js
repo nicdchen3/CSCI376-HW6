dbd9cbb6a61ca97a0b3427ac1c6e478771d7c6aa
lsouth-williams <ls22@williams.edu>
2025-05-05 10:36:19 -0400

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
-images = document.querySelectorAll("img")
-
 window.addEventListener("load", function(e){
+    images = document.querySelectorAll("img")
     images.forEach(function(image){
         image.src = "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"
     })
